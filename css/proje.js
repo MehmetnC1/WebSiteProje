509f113c43bb5bc827c6c8eea37cd57224c70dea
MehmetnC1 <coskunnazim108@gmail.com>
2024-12-27 08:08:59 +0300

    Add files via upload

---
+++ css/proje.js
@@ -0,0 +1,8 @@
+
+  document.querySelector('.aramamotoru').addEventListener('focus', function() {
+    this.style.backgroundColor = '#f0f8ff';
+  });
+
+  document.querySelector('.aramamotoru').addEventListener('blur', function() {
+    this.style.backgroundColor = '';
+  });
